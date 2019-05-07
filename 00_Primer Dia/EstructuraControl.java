ab56803bb905044f3b73ac6521dc2eda1c79540c
Jorge <jorgekarkour15@gmail.com>
2019-05-07 13:59:23 +0200

    Primer dia en Escuela Java, ejemplos basicos, estructuras de control y tucanes y otros pajaros (INTRO A POO)

---
+++ 00_Primer Dia/EstructuraControl.java
@@ -0,0 +1,13 @@
+
+class EstructurasControl
+{
+	public static void main (String argumentos[]) {
+		int contador = 0;
+		while (contador < 5);{
+		System.out.printl("contado vale" +contador);
+		contador ++;
+		}
+		{
+			System.out.printl("Bloque");
+			int var = 0
+		}
